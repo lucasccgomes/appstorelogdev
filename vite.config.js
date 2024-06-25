5b48a399c7217b7cab5ca46aa113c761a6c9048b
Lucasccgomes <lucasccgomes@protonmail.com>
2024-06-25 16:53:39 -0300

    feat:update and config icons

--- vite.config.js
+++ vite.config.js
@@ -12,20 +12,59 @@ export default defineConfig({
         enabled: true
       },
       manifest: {
+        id: "/",
         name: 'Meu App',
         short_name: 'App',
         description: 'Meu Progressive Web App',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        orientation: 'portrait',
         icons: [
           {
-            src: 'icons/icon-192x192.png',
+            src: 'icons/android-launchericon-48-48.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/android-launchericon-72-72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/android-launchericon-96-96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/android-launchericon-144-144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/android-launchericon-192-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: 'icons/android-launchericon-512-512.png',
             sizes: '512x512',
             type: 'image/png'
+          }
+        ],
+        screenshots: [
+          {
+            src: 'screenshots/screenshot-1.png',
+            sizes: '1440x900',
+            type: 'image/png',
+            form_factor: 'wide'
+          },
+          {
+            src: "screenshots/screenshot-2.png",
+            sizes: "398x712",
+            type: "image/png",
+            form_factor: "narrow"
           }
         ]
       }
